1039c43ddae02af954f7ac338afae41864e2c772
Ray_1024 <skovorodnikovda@mail.ru>
2024-10-29 00:24:18 +0300

    Rename controllers to more informally

--- src/main/java/com/rdbaa/controller/ErrorRedirectController.java
+++ src/main/java/com/rdbaa/controller/ErrorRedirectController.java
@@ -4,21 +4,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.io.IOException;
 
 @RestController
-public class RoutesController implements ErrorController {
+public class ErrorRedirectController implements ErrorController {
     private static final String PATH = "/error";
-
 
     @RequestMapping(value = PATH)
     public void error(HttpServletResponse response) throws IOException {
         response.sendRedirect("/");
-    }
-
-    @Override
-    public String getErrorPath() {
-        return PATH;
     }
 }
 
--- src/main/java/com/rdbaa/controller/MainPageController.java
+++ src/main/java/com/rdbaa/controller/MainPageController.java
@@ -3,7 +3,7 @@ package com.rdbaa.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
-public class Controller {
+public class MainPageController {
     @GetMapping
     public String choosePage(){
         return "index.html";
