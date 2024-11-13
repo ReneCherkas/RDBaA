a720faaac6d11f53a62f49e23f4a94a87c6cba66
Ray_1024 <skovorodnikovda@mail.ru>
2024-11-14 00:04:59 +0300

    Add strategy endpoints and request/response DTOs

--- src/main/java/com/rdbaa/controller/StrategyController.java
+++ src/main/java/com/rdbaa/controller/StrategyController.java
@@ -1,8 +1,11 @@
 package com.rdbaa.controller;
 
+import com.rdbaa.model.response.CharacterStrategyResponse;
+import com.rdbaa.model.response.WeaponStrategyResponse;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
@@ -10,4 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class StrategyController {
     private static final Logger logger = LoggerFactory.getLogger(StrategyController.class);
 
+    @GetMapping("/strategy/character")
+    public CharacterStrategyResponse getCharacterStrategy() {
+        return new CharacterStrategyResponse();
+    }
+
+    @GetMapping("/strategy/weapon")
+    public WeaponStrategyResponse getWeaponStrategy() {
+        return new WeaponStrategyResponse();
+    }
 }
---
+++ src/main/java/com/rdbaa/model/response/CharacterStrategyResponse.java
@@ -0,0 +1,13 @@
+package com.rdbaa.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CharacterStrategyResponse {
+}
